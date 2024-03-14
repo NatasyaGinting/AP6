0c3c5d259ba46c93078dac4c1c26b104366ce6d8
NatasyaGinting <162658362+NatasyaGinting@users.noreply.github.com>
2024-03-14 15:29:08 +0700

    Add files via upload

---
+++ operasi.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+
+    //assignment operator masukkan data ke var
+    a = 5;
+    b = 7;
+
+    //arithematic operator
+    int tambah = a + b;
+    int kurang = a - b;
+    int kali = a * b;
+    int bagi = a / b;
+    int mod = a % b;
+
+    cout <<" a + b = " << tambah << endl;
+    cout <<" a - b = " << kurang << endl;
+    cout <<" a * b = " << kali << endl;
+    cout <<" a / b = " << bagi << endl;
+    cout <<" a % b = " << mod << endl;
+
+    //relational operator
+    cout << (a == b) << endl;
+    cout << (a < b) << endl;
+    cout << (a <= b) << endl;
+    cout << (a > b) << endl;
+    cout << (a >= b) << endl;
+    cout << (a != b) << endl;
+
+    //logical operator
+    cout << (true && true) << endl;
+    cout << (true && false) << endl;
+    cout << (false && true) << endl;
+    cout << (false && false) << endl;
+
+    cout << (true || true) << endl;
+    cout << (true || false) << endl;
+    cout << (false || true) << endl;
+    cout << (false || false) << endl;
+
+    cout << !true << endl;
+    cout << !false << endl;
+
+    //bitwise operator
+    cout << (4 & 7) << endl;
+    cout << (4 | 7) << endl;
+    cout << (4 ^ 7) << endl;
+    cout << (~4) << endl;
+    cout << (4 << 7) << endl;
+    cout << (4 >> 7) << endl;
+
+    //shorthand
+    a = a + 2;
+    a += 2;
+    a -= 2;
+    a *= 2;
+    a/b
+
+    b = b + 2;
+    b/=
+
+    //increment & decrement operator
+    //post increment
+
+    //pre increment
+
+    //post decrement
+
+    //pre decrement
+
+    }
